18757c0282837a12c6a3ea7c0b88a2828c40344e
forestbro <alp9dev@proton.me>
2024-11-21 17:28:39 +0200

    initialize database connection, create empty tables

---
+++ TestScripts/init_db.py
@@ -0,0 +1,44 @@
+import psycopg2
+
+db_config = {
+    "dbname": "ids",
+    "user": "postgres",
+    "password": "postgres",
+    "host": "localhost",
+    "port": 5432   
+}
+
+create_researchers_table = """
+CREATE TABLE IF NOT EXISTS researchers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+"""
+
+create_vectors_table = """
+CREATE TABLE IF NOT EXISTS vectors (
+    id SERIAL PRIMARY KEY,
+    researcher_id INTEGER REFERENCES researchers(id),
+    vector DOUBLE PRECISION[]
+);
+"""
+
+def create_tables():
+    try:
+        conn = psycopg2.connect(**db_config)
+        cursor = conn.cursor()
+
+        cursor.execute(create_researchers_table)
+        cursor.execute(create_vectors_table)
+
+        conn.commit()
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+if __name__ == "__main__":
+    create_tables()
